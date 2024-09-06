7b0b2e3380bf5e317ff3bd18e7976516b3b70f58
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-09-06 23:38:16 +0800

    feat: add auth

--- config/config/settings.py
+++ config/config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authz',
     'sortinghat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authz.User'
