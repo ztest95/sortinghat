5c82de6295e315d1b3b9a0c5b999514b854223c3
ztest95 <110767420+ztest95@users.noreply.github.com>
2025-10-21 18:20:23 +0800

    fixup! feat: demo on landing page

--- config/config/urls.py
+++ config/config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from sortinghat.views import sortinghat
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authz.urls')),
     path('', include('link.urls')),
-    path('', include('sortinghat.urls')), # unauth app
+    path('', sortinghat, name='sortinghat'), # unauthenticated sortinghat
 ]
